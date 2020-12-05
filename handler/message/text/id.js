04a13c3f70facad8f85bc4d8fca1cd7d1fd39a66
Rizki-Hi <74732749+Rizki-Hi@users.noreply.github.com>
2020-12-05 19:03:22 +0700

    Update id.js

--- handler/message/text/id.js
+++ handler/message/text/id.js
@@ -1,1 +1,89 @@
+exports.textMenu = (pushname) => {
+    kembali '
+Halo $ {pushname}! 👋️
+Berikut ini beberapa fitur dari bot ini! ✨
 
+Pembuat Stiker:
+𐃘 *#stiker*
+Untuk mengubah gambar menjadi figurine. 
+Penggunaan: kirim gambar dengan subtitle #sticker atau balas gambar yang dikirim dengan #sticker
+
+𐃘 *#stiker* _ <URL gambar> _
+*Untuk mengubah gambar url menjadi stiker.*
+*Cara Menggunakan: Kirim gambar dengan subtitle #sticker atau balas gambar yang dikirim dengan #stiker*
+
+𐃘 *#gifsticker* _ <Giphy URL> _ / *#stickergif* _ <Giphy URL> _
+*Untuk mengubah GIF menjadi stiker (khusus Giphy)*
+*Penggunaan: Kirim pesan dengan format ini: gifsticker https://media.giphy.com/media/JUvI2c1ddyzkwK4RlV/giphy.gif*
+
+𐃘 *#memesticker* _ <teks menit> _ | _ <teks bawah> _
+*Untuk membuat meme stiker dengan teks atas dan bawah*
+*Penggunaan: Kirim gambar dengan keterangan: #meme top me | atau Anda juga dapat menanggapi gambar yang sudah ada.*
+
+*Unduh Video Dari Jejaring Sosial:*
+
+𐃘 *#tiktok * _ <tiktok url> _
+*Untuk mengunduh video dari video tiktok.*
+*Penggunaan: Kirim pesan dengan format ini: #tiktok https://www.tiktok.com/@itsandani/video/6869248690381425922*
+
+𐃘 *#fb* _ <post / video url> _
+*Untuk mengunduh video dari Facebook.*
+*Penggunaan: Kirim pesan dengan format ini: #fb https://www.facebook.com/.....*
+
+𐃘 *#ig* _ <url posting instagram> _
+*Untuk mengunduh foto dan video dari Instagram.*
+*Penggunaan: Kirim pesan dengan format ini: #ig https://www.instagram.com/p/BPOd1vhDMIp/*
+
+𐃘 *#twt* _ <url posting twitter> _
+*Untuk mengunduh foto dan video dari Twitter.*
+*Penggunaan: Kirim pesan dengan format ini: #twt https://twitter.com/ntsana_/status/1306108656887324672*
+
+Dll:
+
+𐃘 *# translate* _ <kode bahasa> _
+* Untuk menafsirkan pesan dalam bahasa yang ditentukan.*
+* Penggunaan: Balas / kutipan / balas pesan yang ingin Anda terjemahkan dengan #translate id _ <- id adalah kode bahasanya. kode bahasanya dapat dilihat di https://bit.ly/33FVldE*
+
+𐃘 *#resi* _ <kurir> _ _ <nomer resi> _
+Untuk memeriksa status pengiriman pembelian Anda (hanya kantor pos)
+Penggunaan: Kirim pesan dengan format ini: #resicorreios seucpf* 
+
+𐃘 *#meme* _ <teks menit> _ | _ <teks bawah> _
+*Untuk membuat meme dengan teks atas dan bawah*
+*Penggunaan: Kirim gambar dengan keterangan: #meme texto | atau Anda juga dapat menanggapi gambar yang sudah ada.*
+
+
+𐃘 *#tnc*
+*Menampilkan syarat dan ketentuan bot.*
+
+
+
+*Saya harap hari Anda menyenangkan!* ✨`
+}
+
+exports.textAdmin = () => {
+    kembali '
+⚠ [*Hanya grup administrator*] ⚠
+*Berikut adalah beberapa fitur administrasi grup yang termasuk dalam bot ini!*
+
+𐃘 *#tendangan* @numero
+*Menghapus anggota dari grup (bisa lebih dari 1).*
+
+𐃘 *#promosikan* @numero
+*Promosikan anggota menjadi administrator grup.*
+
+𐃘 *#turunkan* @numero
+*Turunkan administrator grup.*
+
+𐃘 *#tagall*
+*Sebutkan semua anggota grup.*
+}
+
+𐃘 *#del*
+Untuk menghapus pesan dari robot (balas pesan bot dengan #del)`
+}
+
+exports.textDonasi = () => {
+    return '
+Terima kasih telah menggunakan robot saya ❤
+}
